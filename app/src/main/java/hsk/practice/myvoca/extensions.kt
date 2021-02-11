05e666c4a5c11e1ef41090d4007e3606928d968b
hsk <mwy3055@gmail.com>
2021-02-11 16:50:46 +0900

    feature: library code for waiting a value of LiveData with blocking context

--- app/src/main/java/hsk/practice/myvoca/extensions.kt
+++ app/src/main/java/hsk/practice/myvoca/extensions.kt
@@ -1,11 +1,15 @@
 package hsk.practice.myvoca
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
 
 /**
- * Checks if the given string contains only alphabet
+ * Checks if the given string contains only alphabet.
  *
- * @param str string to check
- * @return true if string contains only alphabet, false otherwise
+ * @return `true` if string contains only alphabet, `false` otherwise
  */
 fun String?.containsOnlyAlphabet(): Boolean {
     if (this.isNullOrEmpty()) return false
@@ -15,4 +19,18 @@ fun String?.containsOnlyAlphabet(): Boolean {
         }
     }
     return true
+}
+
+
+@Throws(Exception::class)
+fun <T> LiveData<T>.getValueBlocking(): T {
+    var value: T? = null
+    val latch = CountDownLatch(1)
+    val innerObserver = Observer<T> {
+        value = it
+        latch.countDown()
+    }
+    observeForever(innerObserver)
+    latch.await(10, TimeUnit.SECONDS)
+    return value ?: throw IllegalStateException("Value not set")
 }
