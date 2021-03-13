e5ca305bb0bcba82c4cac28c767b03622a755398
hsk <mwy3055@gmail.com>
2021-03-13 23:07:30 +0900

    VocaViewModelFactory is able to create SeeAllViewModel instance

--- app/src/main/java/hsk/practice/myvoca/ui/VocaViewModelFactory.kt
+++ app/src/main/java/hsk/practice/myvoca/ui/VocaViewModelFactory.kt
@@ -5,13 +5,15 @@ import androidx.lifecycle.ViewModelProvider
 import com.hsk.data.VocaPersistence
 import hsk.practice.myvoca.ui.home.HomeViewModel
 import hsk.practice.myvoca.ui.quiz.QuizViewModel
+import hsk.practice.myvoca.ui.seeall.SeeAllViewModel
 import java.lang.reflect.InvocationTargetException
 
 class VocaViewModelFactory(private val vocaPersistence: VocaPersistence) : ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         try {
             return if (modelClass.isAssignableFrom(HomeViewModel::class.java)
-                    || modelClass.isAssignableFrom(QuizViewModel::class.java)) {
+                    || modelClass.isAssignableFrom(QuizViewModel::class.java)
+                    || modelClass.isAssignableFrom(SeeAllViewModel::class.java)) {
                 modelClass.getConstructor(VocaPersistence::class.java).newInstance(vocaPersistence)
             } else {
                 super.create(modelClass)
