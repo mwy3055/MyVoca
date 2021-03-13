17dce26a7f0a5cf2a4c9f4c78b4ce8f612112815
hsk <mwy3055@gmail.com>
2021-03-13 23:53:46 +0900

    refactor: 간단한 코드 리팩토링 (줄 간격 등)

--- app/src/main/java/hsk/practice/myvoca/ui/seeall/OnVocabularyUpdateListener.kt
+++ app/src/main/java/hsk/practice/myvoca/ui/seeall/OnVocabularyUpdateListener.kt
@@ -1,11 +1,9 @@
 package hsk.practice.myvoca.ui.seeall
 
-import hsk.practice.myvoca.framework.RoomVocabulary
-
 /**
  * Custom listener which defines a behavior when an item is edited (mainly in VocaRecyclerView).
  * Implemented at SeeAllFragment.editVocabulary().
  */
 interface OnVocabularyUpdateListener {
-    fun editVocabulary(position: Int, vocabulary: RoomVocabulary?)
+    fun updateVocabulary(position: Int)
 }
