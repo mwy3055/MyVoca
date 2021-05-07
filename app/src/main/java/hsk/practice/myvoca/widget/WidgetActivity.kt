579184f5ca2d39093b3b6b7f367a98e4375d98c4
hsk <mwy3055@gmail.com>
2021-05-07 21:38:46 +0900

    feature: Widget configuration activity에 닫기 버튼 추가

--- app/src/main/java/hsk/practice/myvoca/widget/WidgetActivity.kt
+++ app/src/main/java/hsk/practice/myvoca/widget/WidgetActivity.kt
@@ -22,24 +22,30 @@ class WidgetActivity : AppCompatActivity() {
     @Inject
     lateinit var vocaRepository: VocaRepository
 
+    private var widgetId: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityWidgetSettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val widgetId = intent.getIntExtra(
+        binding.buttonClose.setOnClickListener {
+            closeActivity()
+        }
+
+        widgetId = intent.getIntExtra(
             AppWidgetManager.EXTRA_APPWIDGET_ID,
             AppWidgetManager.INVALID_APPWIDGET_ID
         )
-        showWidget(widgetId)
+        showWidget()
     }
 
     /**
      * Widget configuration Activity가 정의되었으므로,
      * 위젯을 생성한 후 최초의 업데이트는 여기에서 수행해야 한다.
      */
-    private fun showWidget(widgetId: Int) = lifecycleScope.launch {
+    private fun showWidget() = lifecycleScope.launch {
         Logger.d("WidgetActivity showWidget()")
         val context = this@WidgetActivity
         val widgetManager = AppWidgetManager.getInstance(context)
@@ -49,7 +55,9 @@ class WidgetActivity : AppCompatActivity() {
             .also { remoteViews ->
                 widgetManager.updateAppWidget(widgetId, remoteViews)
             }
+    }
 
+    private fun closeActivity() {
         val result = Intent().apply {
             putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId)
         }
