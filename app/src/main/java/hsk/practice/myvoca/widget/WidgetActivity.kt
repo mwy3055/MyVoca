107b4e46faa60cb9d18fc2d6c05b830eea03ab5d
hsk <mwy3055@gmail.com>
2021-05-07 22:03:56 +0900

    feature: 위젯을 설정하는 동안 로딩 이미지를 보여줌

--- app/src/main/java/hsk/practice/myvoca/widget/WidgetActivity.kt
+++ app/src/main/java/hsk/practice/myvoca/widget/WidgetActivity.kt
@@ -3,6 +3,7 @@ package hsk.practice.myvoca.widget
 import android.appwidget.AppWidgetManager
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.hsk.data.VocaRepository
@@ -11,8 +12,10 @@ import dagger.hilt.android.AndroidEntryPoint
 import hsk.practice.myvoca.databinding.ActivityWidgetSettingBinding
 import hsk.practice.myvoca.framework.toRoomVocabulary
 import hsk.practice.myvoca.module.RoomVocaRepository
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.math.min
 
 @AndroidEntryPoint
 class WidgetActivity : AppCompatActivity() {
@@ -34,6 +37,10 @@ class WidgetActivity : AppCompatActivity() {
             closeActivity()
         }
 
+        setProgressSize()
+        setProgressVisibility(View.VISIBLE)
+        setTextButtonVisibility(View.GONE)
+
         widgetId = intent.getIntExtra(
             AppWidgetManager.EXTRA_APPWIDGET_ID,
             AppWidgetManager.INVALID_APPWIDGET_ID
@@ -46,15 +53,23 @@ class WidgetActivity : AppCompatActivity() {
      * 위젯을 생성한 후 최초의 업데이트는 여기에서 수행해야 한다.
      */
     private fun showWidget() = lifecycleScope.launch {
-        Logger.d("WidgetActivity showWidget()")
-        val context = this@WidgetActivity
-        val widgetManager = AppWidgetManager.getInstance(context)
+        val waitingJob = launch { delay(800L) }
+        launch {
+            Logger.d("WidgetActivity showWidget()")
+            val context = this@WidgetActivity
+            val widgetManager = AppWidgetManager.getInstance(context)
 
-        val vocabulary = vocaRepository.getRandomVocabulary()?.toRoomVocabulary() ?: return@launch
-        VocaWidgetProvider.remoteViewWithVocabulary(context, widgetId, vocabulary)
-            .also { remoteViews ->
-                widgetManager.updateAppWidget(widgetId, remoteViews)
-            }
+            val vocabulary =
+                vocaRepository.getRandomVocabulary()?.toRoomVocabulary()
+                    ?: throw IllegalStateException("Vocabulary is null!")
+            VocaWidgetProvider.remoteViewWithVocabulary(context, widgetId, vocabulary)
+                .also { remoteViews ->
+                    widgetManager.updateAppWidget(widgetId, remoteViews)
+                }
+            waitingJob.join()
+            setProgressVisibility(View.GONE)
+            setTextButtonVisibility(View.VISIBLE)
+        }
     }
 
     private fun closeActivity() {
@@ -63,5 +78,24 @@ class WidgetActivity : AppCompatActivity() {
         }
         setResult(RESULT_OK, result)
         finish()
+    }
+
+    private fun setProgressSize() {
+        val metrics = resources.displayMetrics
+        val size = min(metrics.widthPixels, metrics.heightPixels) / 2
+        val params = binding.progressLoading.layoutParams
+        params.width = size
+        params.height = size
+    }
+
+    private fun setProgressVisibility(visibility: Int) {
+        binding.progressLoading.visibility = visibility
+    }
+
+    private fun setTextButtonVisibility(visibility: Int) {
+        with(binding) {
+            textAdd.visibility = visibility
+            buttonClose.visibility = visibility
+        }
     }
 }
