02253633fa71467765d687f640520dff6b86b178
hsk <mwy3055@gmail.com>
2021-05-07 22:58:00 +0900

    feature: 위젯 설정 화면에서 텍스트의 색깔을 선택할 수 있음
    색깔을 선택할 수 있는 ColorPicker를 추가하여 텍스트의 색깔을 선택할 수 있도록 함

--- app/src/main/java/hsk/practice/myvoca/widget/WidgetActivity.kt
+++ app/src/main/java/hsk/practice/myvoca/widget/WidgetActivity.kt
@@ -5,10 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
+import com.flask.colorpicker.ColorPickerView
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder
 import com.hsk.data.VocaRepository
 import com.orhanobut.logger.Logger
 import dagger.hilt.android.AndroidEntryPoint
+import hsk.practice.myvoca.R
 import hsk.practice.myvoca.databinding.ActivityWidgetSettingBinding
 import hsk.practice.myvoca.framework.toRoomVocabulary
 import hsk.practice.myvoca.module.RoomVocaRepository
@@ -27,25 +31,34 @@ class WidgetActivity : AppCompatActivity() {
 
     private var widgetId: Int = 0
 
+    private var textColor = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityWidgetSettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonClose.setOnClickListener {
-            closeActivity()
+        setColorPickerSize()
+        setProgressSize()
+
+        setColorPickerVisibility(View.VISIBLE)
+        setProgressVisibility(View.GONE)
+        setTextButtonVisibility(View.GONE)
+
+        binding.buttonClose.setOnClickListener { closeActivity() }
+        binding.imageColorPreview.setOnClickListener { showColorPicker() }
+        binding.buttonColorComplete.setOnClickListener {
+            setColorPickerVisibility(View.GONE)
+            showWidget()
         }
 
-        setProgressSize()
-        setProgressVisibility(View.VISIBLE)
-        setTextButtonVisibility(View.GONE)
+        textColor = ContextCompat.getColor(this, R.color.material_light_primary_text)
 
         widgetId = intent.getIntExtra(
             AppWidgetManager.EXTRA_APPWIDGET_ID,
             AppWidgetManager.INVALID_APPWIDGET_ID
         )
-        showWidget()
     }
 
     /**
@@ -80,12 +93,41 @@ class WidgetActivity : AppCompatActivity() {
         finish()
     }
 
+    private fun showColorPicker() {
+        ColorPickerDialogBuilder.with(this)
+            .setTitle("텍스트 색깔")
+            .initialColor(textColor)
+            .wheelType(ColorPickerView.WHEEL_TYPE.CIRCLE).density(10)
+            .setPositiveButton("확인") { _, lastSelectedColor, _ ->
+                textColor = lastSelectedColor
+                binding.imageColorPreview.setBackgroundColor(lastSelectedColor)
+            }
+            .setNegativeButton("취소") { _, _ -> }
+            .build()
+            .show()
+    }
+
+    private fun setColorPickerSize() {
+        val metrics = resources.displayMetrics
+        val params = binding.imageColorPreview.layoutParams
+        params.width = metrics.widthPixels / 2
+        params.height = metrics.widthPixels / 11
+    }
+
     private fun setProgressSize() {
         val metrics = resources.displayMetrics
         val size = min(metrics.widthPixels, metrics.heightPixels) / 2
         val params = binding.progressLoading.layoutParams
         params.width = size
         params.height = size
+    }
+
+    private fun setColorPickerVisibility(visibility: Int) {
+        with(binding) {
+            textColorPick.visibility = visibility
+            imageColorPreview.visibility = visibility
+            buttonColorComplete.visibility = visibility
+        }
     }
 
     private fun setProgressVisibility(visibility: Int) {
