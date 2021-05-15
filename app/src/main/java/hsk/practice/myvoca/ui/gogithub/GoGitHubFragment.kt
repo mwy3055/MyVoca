f2551a42b56e306a21efb7dc4c092b845475528b
hsk <mwy3055@gmail.com>
2021-05-15 21:36:06 +0900

    refactor: GitHub으로 이동하는 프래그먼트 코드 리팩토링
    Fragment에서는 XML 상에서 android:onClick을 설정할 수 없다.설정한 메소드를 Activity에서 찾기 때문이다.

--- app/src/main/java/hsk/practice/myvoca/ui/gogithub/GoGitHubFragment.kt
+++ app/src/main/java/hsk/practice/myvoca/ui/gogithub/GoGitHubFragment.kt
@@ -12,8 +12,8 @@ import dagger.hilt.android.AndroidEntryPoint
 import hsk.practice.myvoca.databinding.FragmentGoGithubBinding
 
 /**
- * Just for fun.
- * Maybe some features can be added here...
+ * Go to the GitHub repository of the MyVoca.
+ * Just for fun, maybe something interesting can be added here.
  */
 @AndroidEntryPoint
 class GoGitHubFragment : Fragment() {
@@ -23,16 +23,16 @@ class GoGitHubFragment : Fragment() {
 
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentGoGithubBinding.inflate(inflater, container, false)
         binding.goGitHubViewModel = goGitHubViewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
         binding.buttonGoGithub.setOnClickListener {
-            val url = "https://github.com/mwy3055/MyVoca/issues"
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            startActivity(intent)
+            exploreGitHub()
         }
 
         return binding.root
@@ -41,5 +41,11 @@ class GoGitHubFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun exploreGitHub() {
+        val url = "https://github.com/mwy3055/MyVoca/issues"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 }
