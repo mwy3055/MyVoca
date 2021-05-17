a81c73833450a0e98230a223194c751f4b77c82e
hsk <mwy3055@gmail.com>
2021-05-17 22:05:40 +0900

    feature: VersusView에 Data binding 적용
    이제 VersusView 전용 ViewModel이 값을 제공하며, Data binding을 이용해 UI에 적용한다.

--- app/src/main/java/hsk/practice/myvoca/ui/customview/VersusView.kt
+++ app/src/main/java/hsk/practice/myvoca/ui/customview/VersusView.kt
@@ -7,44 +7,20 @@ import android.widget.LinearLayout
 import hsk.practice.myvoca.databinding.VersusViewBinding
 
 /**
- * Custom view which compares two values and show proper graphics.
- * Hard to explain in text, just see the QuizFragment!
+ * Custom view which compares two values and show them with bar graph.
+ * Hard to explain in text, just see the design!
  */
-class VersusView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
-    : LinearLayout(context, attrs, defStyleAttr) {
+class VersusView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
 
-    private val binding = VersusViewBinding.inflate(LayoutInflater.from(context), this, true)
-
-    private val BAR_HEIGHT = 50
-    private var leftValue = 0
-    private var rightValue = 0
-
-    fun setValues(left: Int, right: Int) {
-        setLeftValue(left)
-        setRightValue(right)
+    val binding = VersusViewBinding.inflate(LayoutInflater.from(context), this, true).apply {
+        viewModel = VersusViewModel()
     }
 
-    fun setLeftValue(value: Int) {
-        if (value < 0) {
-            return
-        }
-        leftValue = value
-        binding.countLeft.text = leftValue.toString()
-        refreshView()
-    }
+    val viewModel: VersusViewModel
+        get() = binding.viewModel!!
 
-    fun setRightValue(value: Int) {
-        if (value < 0) {
-            return
-        }
-        rightValue = value
-        binding.countRight.text = rightValue.toString()
-        refreshView()
-    }
-
-    fun refreshView() {
-        // Weights should be set oppositely to get correct result
-        binding.leftBar.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, BAR_HEIGHT, rightValue.toFloat())
-        binding.rightBar.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, BAR_HEIGHT, leftValue.toFloat())
-    }
 }
