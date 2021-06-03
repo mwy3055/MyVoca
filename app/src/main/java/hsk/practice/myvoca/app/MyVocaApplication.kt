6f1d411719e57f184b42958b49606f210cf2e0d7
hsk <mwy3055@gmail.com>
2021-06-03 20:09:03 +0900

    refactor: 애플리케이션 초기화 코드를 Application에서 실행하도록 수정

--- app/src/main/java/hsk/practice/myvoca/app/MyVocaApplication.kt
+++ app/src/main/java/hsk/practice/myvoca/app/MyVocaApplication.kt
@@ -1,7 +1,18 @@
 package hsk.practice.myvoca.app
 
 import android.app.Application
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyVocaApplication : Application()
+class MyVocaApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        loadLogger()
+    }
+
+    private fun loadLogger() {
+        Logger.addLogAdapter(AndroidLogAdapter())
+    }
+}
