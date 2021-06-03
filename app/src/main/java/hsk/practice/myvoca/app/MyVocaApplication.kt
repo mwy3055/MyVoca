dcaf1efbe81733cae67b5a4ab9dba474287c725f
hsk <mwy3055@gmail.com>
2021-06-03 23:05:22 +0900

    feature: 다크 모드 설정 기능 추가
    다크 모드 설정을 변경하면 변경한 내용이 즉시 적용된다.

--- app/src/main/java/hsk/practice/myvoca/app/MyVocaApplication.kt
+++ app/src/main/java/hsk/practice/myvoca/app/MyVocaApplication.kt
@@ -1,18 +1,28 @@
 package hsk.practice.myvoca.app
 
 import android.app.Application
+import androidx.preference.PreferenceManager
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
 import dagger.hilt.android.HiltAndroidApp
+import hsk.practice.myvoca.R
+import hsk.practice.myvoca.setNightMode
 
 @HiltAndroidApp
 class MyVocaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         loadLogger()
+        setDarkModeLayout()
     }
 
     private fun loadLogger() {
         Logger.addLogAdapter(AndroidLogAdapter())
+    }
+
+    private fun setDarkModeLayout() {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val darkMode = sharedPreferences.getBoolean(getString(R.string.settings_dark_mode), false)
+        setNightMode(darkMode)
     }
 }
