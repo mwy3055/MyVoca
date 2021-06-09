bd955e1cdc8eee7ee9a0efb498d27ea839219ac5
hsk <mwy3055@gmail.com>
2021-06-09 21:27:03 +0900

    refactor: DataStore Preferences 전용 클래스 작성
    모든 preference 키는 MyVocaPreferences에 정의되어야 한다. Context의 확장 함수로 정의된 suspend 함수를 호출하여 preference에 접근할 수 있다.

--- app/src/main/java/hsk/practice/myvoca/extensions.kt
+++ app/src/main/java/hsk/practice/myvoca/extensions.kt
@@ -1,17 +1,8 @@
 package hsk.practice.myvoca
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import java.util.*
 
-/**
- * Preferences DataStore object. Delegated by preferenceDataStore().
- * Once this property is initialized, it can be accessed through the whole application.
- */
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 /**
  * Checks if the given string contains only alphabet.
@@ -19,13 +10,8 @@ val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "se
  * @return `true` if string contains only alphabet, `false` otherwise
  */
 fun String?.containsOnlyAlphabet(): Boolean {
-    if (this.isNullOrEmpty()) return false
-    this.forEach {
-        if (it !in 'a'..'z' && it !in 'A'..'Z' && it != '%') {
-            return false
-        }
-    }
-    return true
+    if (isNullOrEmpty()) return false
+    return all { it in 'a'..'z' || it in 'A'..'Z' || it == '%' }
 }
 
 
