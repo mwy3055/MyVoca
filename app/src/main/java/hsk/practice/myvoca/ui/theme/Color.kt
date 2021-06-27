622377f1305ee48f90f930f13ee34bc934b8c985
hsk <mwy3055@gmail.com>
2021-06-27 21:05:03 +0900

    theme: 앱에서 사용할 Theme을 설정
    Theme은 계속 개선하기

--- app/src/main/java/hsk/practice/myvoca/ui/theme/Color.kt
+++ app/src/main/java/hsk/practice/myvoca/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package hsk.practice.myvoca.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
+val Purple300 = Color(0xFFBA68C8)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val Teal700 = Color(0xFF018786)
