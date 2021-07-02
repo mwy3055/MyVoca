3d03bcda46a2ae3b7138fd1c36bfa75ff4dfb919
hsk <mwy3055@gmail.com>
2021-07-02 22:47:23 +0900

    feature: WordClassImpl에 품사(영어)를 한국어로 번역한 변수 추가
    각 품사마다 한국어 뜻을 추가했다.
    ex: NOUN(명사)

--- app/src/main/java/hsk/practice/myvoca/data/VocabularyImpl.kt
+++ app/src/main/java/hsk/practice/myvoca/data/VocabularyImpl.kt
@@ -34,16 +34,16 @@ data class MeaningImpl(
     val content: String
 ) : Serializable
 
-enum class WordClassImpl {
-    NOUN,
-    PRONOUN,
-    VERB,
-    ADJECTIVE,
-    ADVERB,
-    PREPOSITION,
-    CONJUNCTION,
-    INTERJECTION,
-    UNKNOWN
+enum class WordClassImpl(val korean: String) {
+    NOUN("명사"),
+    PRONOUN("대명사"),
+    VERB("동사"),
+    ADJECTIVE("형용사"),
+    ADVERB("부사"),
+    PREPOSITION("전치사"),
+    CONJUNCTION("접속사"),
+    INTERJECTION("감탄사"),
+    UNKNOWN("???")
 }
 
 /**
