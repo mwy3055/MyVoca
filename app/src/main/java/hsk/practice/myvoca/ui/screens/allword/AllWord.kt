b7a6e081ed814d15696bb4013209b4422b1d9391
hsk <mwy3055@gmail.com>
2021-07-06 21:24:48 +0900

    feature: 전체 단어를 보여주는 AllWordItems 구현 완료
    이전에 작성된 WordContent를 활용하여 단어를 보여주는 AllWordItems composable을 작성하였다. 하지만 뷰 내용을 보여주기 직전에 살짝 멈춤 현상이 있어 이를 개선해야 한다.

--- app/src/main/java/hsk/practice/myvoca/ui/screens/allword/AllWord.kt
+++ app/src/main/java/hsk/practice/myvoca/ui/screens/allword/AllWord.kt
@@ -1,49 +1,90 @@
 package hsk.practice.myvoca.ui.screens.allword
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import hsk.practice.myvoca.data.VocabularyImpl
+import hsk.practice.myvoca.data.fakeData
+import hsk.practice.myvoca.ui.components.WordContent
 import hsk.practice.myvoca.ui.state.UiState
 import hsk.practice.myvoca.ui.theme.MyVocaTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun AllWordScreen(
     allWordViewModel: AllWordViewModel = viewModel()
 ) {
+    val coroutineScope = rememberCoroutineScope()
     val allWordUiState by allWordViewModel.allWordUiState.collectAsState()
 
-    AllWordLoading(allWordUiState)
+    AllWordLoading(
+        uiState = allWordUiState,
+        onRefresh = { coroutineScope.launch { allWordViewModel.refreshChannel.send(Unit) } }
+    )
 }
 
 @Composable
 fun AllWordLoading(
-    uiState: UiState<AllWordData>
+    uiState: UiState<AllWordData>,
+    onRefresh: () -> Unit = {}
 ) {
-    if (uiState.initialLoad) {
-        /* TODO: add full-screen loading composable.
-            If it is not initial load, just show the on-screen loading composable. */
-        Text(text = "Loading!")
-    } else {
-        AllWordContent(uiState.data!!)
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (uiState.loading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+        uiState.data?.let { data ->
+            AllWordContent(
+                data = data,
+                onRefresh = onRefresh
+            )
+        }
     }
 }
 
 @Composable
 fun AllWordContent(
-    data: AllWordData
+    data: AllWordData,
+    onRefresh: () -> Unit
 ) {
-    Text(
-        text = "${data.currentWordState.size} vocabularies loaded."
-    )
+    Column {
+        Text(
+            text = "${data.currentWordState.size} vocabularies loaded.",
+            modifier = Modifier.clickable(onClick = onRefresh)
+        )
+        Spacer(modifier = Modifier.padding(bottom = 16.dp))
+        AllWordItems(data.currentWordState)
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun AllWordItems(words: List<VocabularyImpl>) {
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        items(words) { word ->
+            WordContent(word)
+        }
+    }
 }
 
 @Preview
 @Composable
-fun AllWordPreview() {
+fun AllWordItemsPreview() {
     MyVocaTheme {
-        AllWordScreen()
+        AllWordItems(fakeData)
     }
 }
