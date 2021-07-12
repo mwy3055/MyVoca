bf75c1f958664c5c2b730d60e59de4af742667bb
hsk <mwy3055@gmail.com>
2021-07-12 23:22:04 +0900

    feature: AllWord 화면 구현 중
    1. 검색 옵션을 설정할 수 있는 AllWordQueryOptions
    * 단어, 품사를 검색할 수 있다.
    * (구현 예정) 단어 정렬 방법을 알파벳 순, 최신순, 랜덤 순서로 설정할 수 있다.
    
    2. 현재 단어를 보여주는 AllWordItems
    * 우선 단어를 보여주는 부분까지 완성하였다.
    * 단어의 삭제 또는 수정 기능은 나중에 다시 생각하자.

--- app/src/main/java/hsk/practice/myvoca/ui/screens/allword/AllWord.kt
+++ app/src/main/java/hsk/practice/myvoca/ui/screens/allword/AllWord.kt
@@ -1,24 +1,40 @@
 package hsk.practice.myvoca.ui.screens.allword
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.ManageSearch
+import androidx.compose.material.icons.outlined.HighlightOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.hsk.data.vocabulary.VocabularyQuery
+import com.hsk.data.vocabulary.WordClass
 import hsk.practice.myvoca.data.VocabularyImpl
+import hsk.practice.myvoca.data.WordClassImpl
 import hsk.practice.myvoca.data.fakeData
+import hsk.practice.myvoca.data.toWordClass
 import hsk.practice.myvoca.ui.components.WordContent
 import hsk.practice.myvoca.ui.state.UiState
 import hsk.practice.myvoca.ui.theme.MyVocaTheme
@@ -32,39 +48,72 @@ fun AllWordScreen(
 
     AllWordLoading(
         uiState = allWordUiState,
-        onSearchOptionClicked = allWordViewModel::onSearchOptionClicked
+        onOptionButtonClicked = allWordViewModel::toggleOptionVisibility,
+        onSubmitButtonClicked = allWordViewModel::onNewQuerySubmit,
+        onCancelButtonClicked = allWordViewModel::toggleOptionVisibility,
+        onQueryWordChanged = allWordViewModel::onQueryTextChanged,
+        onOptionWordClassClick = allWordViewModel::onQueryToggleWordClass
     )
 }
 
 @Composable
 fun AllWordLoading(
     uiState: UiState<AllWordData>,
-    onSearchOptionClicked: () -> Unit
+    onOptionButtonClicked: () -> Unit,
+    onSubmitButtonClicked: () -> Unit,
+    onCancelButtonClicked: () -> Unit,
+    onQueryWordChanged: (String) -> Unit,
+    onOptionWordClassClick: (String) -> Unit
 ) {
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
         if (uiState.loading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .zIndex(1f)
+            )
         }
         uiState.data?.let { data ->
             AllWordContent(
                 data = data,
-                onSearchOptionClicked = onSearchOptionClicked
+                onOptionButtonClicked = onOptionButtonClicked,
+                onSubmitButtonClicked = onSubmitButtonClicked,
+                onCancelButtonClicked = onCancelButtonClicked,
+                onQueryWordChanged = onQueryWordChanged,
+                onOptionWordClassClick = onOptionWordClassClick
             )
         }
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AllWordContent(
     data: AllWordData,
-    onSearchOptionClicked: () -> Unit
+    onOptionButtonClicked: () -> Unit,
+    onSubmitButtonClicked: () -> Unit,
+    onCancelButtonClicked: () -> Unit,
+    onQueryWordChanged: (String) -> Unit,
+    onOptionWordClassClick: (String) -> Unit
 ) {
     Column {
+        AnimatedVisibility(data.optionVisible) {
+            AllWordQueryOptions(
+                modifier = Modifier
+//                    .background(MaterialTheme.colors.primary)
+                    .padding(8.dp),
+                query = data.queryState,
+                onSubmitButtonClicked = onSubmitButtonClicked,
+                onCancelButtonClicked = onCancelButtonClicked,
+                onQueryWordChanged = onQueryWordChanged,
+                onOptionWordClassClick = onOptionWordClassClick
+            )
+        }
         AllWordHeader(
             vocabularySize = data.currentWordState.size,
-            onSearchOptionClicked = onSearchOptionClicked
+            onOptionButtonClicked = onOptionButtonClicked
         )
         AllWordItems(data.currentWordState)
     }
@@ -73,7 +122,7 @@ fun AllWordContent(
 @Composable
 fun AllWordHeader(
     vocabularySize: Int,
-    onSearchOptionClicked: () -> Unit
+    onOptionButtonClicked: () -> Unit
 ) {
     Row(
         modifier = Modifier
@@ -86,12 +135,165 @@ fun AllWordHeader(
                 .weight(1f)
                 .align(Alignment.CenterVertically)
         )
-        TextButton(onClick = onSearchOptionClicked) {
+        TextButton(onClick = onOptionButtonClicked) {
             Icon(
                 imageVector = Icons.Filled.ManageSearch,
                 contentDescription = null
             )
             Text(text = "검색 옵션")
+        }
+    }
+}
+
+@Composable
+fun AllWordQueryOptions(
+    modifier: Modifier = Modifier,
+    query: VocabularyQuery,
+    onSubmitButtonClicked: () -> Unit,
+    onCancelButtonClicked: () -> Unit,
+    onQueryWordChanged: (String) -> Unit,
+    onOptionWordClassClick: (String) -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+//            .padding(8.dp)
+    ) {
+        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            // Buttons
+            AllWordQueryButtons(
+                onCancelButtonClicked = onCancelButtonClicked,
+                onSubmitButtonClicked = onSubmitButtonClicked
+            )
+            AllWordQueryWord(
+                text = query.word,
+                onTextChanged = onQueryWordChanged
+            )
+            AllWordQueryWordClass(
+                selectedWordClass = query.wordClass,
+                onOptionWordClassClick = onOptionWordClassClick
+            )
+        }
+    }
+}
+
+@Composable
+fun AllWordQueryButtons(
+    onCancelButtonClicked: () -> Unit,
+    onSubmitButtonClicked: () -> Unit
+) {
+    val focusManager = LocalFocusManager.current
+    val closeKeyboard = { focusManager.clearFocus() }
+    Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+        TextButton(
+            onClick = {
+                onCancelButtonClicked()
+                closeKeyboard()
+            },
+            modifier = Modifier.weight(1f)
+        ) {
+            Text("닫기")
+        }
+        Button(
+            onClick = {
+                onSubmitButtonClicked()
+                closeKeyboard()
+            },
+            modifier = Modifier.weight(1f)
+        ) {
+            Text("검색")
+        }
+    }
+}
+
+@Composable
+fun AllWordQueryWord(
+    focusManager: FocusManager = LocalFocusManager.current,
+    text: String,
+    onTextChanged: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = text,
+        label = { Text(text = "단어") },
+        trailingIcon = {
+            if (text.isNotEmpty()) {
+                IconButton(onClick = { onTextChanged("") }) {
+                    Icon(
+                        imageVector = Icons.Outlined.HighlightOff,
+                        contentDescription = null
+                    )
+                }
+            }
+        },
+        onValueChange = onTextChanged,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() })
+    )
+}
+
+@Composable
+fun AllWordQueryWordClass(
+    selectedWordClass: Set<WordClass>,
+    onOptionWordClassClick: (String) -> Unit
+) {
+    val classes = WordClassImpl.values().filter { it != WordClassImpl.UNKNOWN }
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        LazyRow {
+            item {
+                WordClassChip(
+                    className = totalWordClassName,
+                    selected = selectedWordClass.isEmpty(),
+                    onClick = onOptionWordClassClick
+                )
+            }
+            items(classes) { wordClass ->
+                WordClassChip(
+                    className = wordClass.korean,
+                    selected = selectedWordClass.contains(wordClass.toWordClass()),
+                    onClick = onOptionWordClassClick
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun WordClassChip(
+    className: String,
+    selected: Boolean,
+    onClick: (String) -> Unit,
+) {
+    val background by animateColorAsState(targetValue = if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.surface)
+    val textColor by animateColorAsState(targetValue = if (selected) MaterialTheme.colors.onPrimary else MaterialTheme.colors.primary)
+
+    Surface(
+        modifier = Modifier
+            .padding(horizontal = 4.dp)
+            .clickable { onClick(className) },
+        elevation = 0.dp,
+        shape = MaterialTheme.shapes.medium,
+        color = background,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AnimatedVisibility(selected) {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = null
+                )
+            }
+            Text(
+                text = className,
+                modifier = Modifier
+                    .padding(6.dp),
+                color = textColor
+            )
         }
     }
 }
@@ -122,6 +324,36 @@ fun AllWordHeaderPreview() {
     MyVocaTheme {
         AllWordHeader(vocabularySize = 10) {
 
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AllWordQueryOptionsPreview() {
+    var word = "test text"
+    val wordClassSet = mutableSetOf<WordClass>()
+
+    MyVocaTheme {
+        AllWordQueryOptions(
+            modifier = Modifier
+                .background(MaterialTheme.colors.surface)
+                .padding(8.dp),
+            query = VocabularyQuery(
+                word = word,
+                wordClass = wordClassSet
+            ),
+            onSubmitButtonClicked = {},
+            onCancelButtonClicked = {},
+            onQueryWordChanged = { word = it }
+        ) { wordClassName ->
+            val wordClass = WordClassImpl.findByKorean(wordClassName)?.toWordClass()
+                ?: return@AllWordQueryOptions
+            if (wordClassSet.contains(wordClass)) {
+                wordClassSet.remove(wordClass)
+            } else {
+                wordClassSet.add(wordClass)
+            }
         }
     }
 }
--- app/src/main/java/hsk/practice/myvoca/ui/screens/allword/AllWordViewModel.kt
+++ app/src/main/java/hsk/practice/myvoca/ui/screens/allword/AllWordViewModel.kt
@@ -8,9 +8,12 @@ import com.hsk.domain.VocaPersistence
 import com.orhanobut.logger.Logger
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hsk.practice.myvoca.data.VocabularyImpl
+import hsk.practice.myvoca.data.WordClassImpl
+import hsk.practice.myvoca.data.toWordClass
 import hsk.practice.myvoca.module.LocalVocaPersistence
 import hsk.practice.myvoca.room.toVocabularyImplList
 import hsk.practice.myvoca.ui.state.UiState
+import hsk.practice.myvoca.xor
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -30,14 +33,16 @@ class AllWordViewModel @Inject constructor(
     val refreshChannel = Channel<Unit>(Channel.CONFLATED)
 
     init {
-        viewModelScope.launch {
-            refreshChannel.send(Unit)
-        }
+        notifyRefresh()
         lookRefreshChannel()
     }
 
     private suspend fun loadWords(query: VocabularyQuery): List<VocabularyImpl> {
         return persistence.getVocabulary(query).toVocabularyImplList()
+    }
+
+    private fun notifyRefresh() = viewModelScope.launch {
+        refreshChannel.send(Unit)
     }
 
     private fun lookRefreshChannel() = viewModelScope.launch {
@@ -61,18 +66,56 @@ class AllWordViewModel @Inject constructor(
     /**
      * Event listeners for composable
      */
-    fun onSearchOptionClicked() {
-        _allWordUiState.value = _allWordUiState.value.copy(
+    fun setOptionVisibility(value: Boolean) {
+        val data = allWordUiState.value.data ?: return
+        _allWordUiState.value = allWordUiState.value.copy(data = data.copy(optionVisible = value))
+    }
 
-        )
+    fun toggleOptionVisibility() {
+        val current = allWordUiState.value.data?.optionVisible ?: return
+        setOptionVisibility(!current)
+    }
+
+    fun onNewQuerySubmit() {
+        notifyRefresh()
+    }
+
+    private fun onQueryChanged(query: VocabularyQuery) {
+        val data = allWordUiState.value.data ?: return
+        val originalQuery = data.queryState
+        if (originalQuery != query) {
+            _allWordUiState.value = allWordUiState.value.copy(data = data.copy(queryState = query))
+        }
+    }
+
+    fun onQueryTextChanged(value: String) {
+        allWordUiState.value.data?.queryState?.let {
+            onQueryChanged(it.copy(word = value))
+        }
+    }
+
+    fun onQueryToggleWordClass(koreanName: String) {
+        allWordUiState.value.data?.queryState?.let { queryState ->
+            val current = queryState.wordClass
+            val new = if (koreanName == totalWordClassName) {
+                emptySet()
+            } else {
+                val wordClass = WordClassImpl.findByKorean(koreanName)?.toWordClass() ?: return
+                current.xor(wordClass).toSet()
+            }
+            onQueryChanged(queryState.copy(wordClass = new))
+        }
     }
 }
 
 @Immutable
 data class AllWordData(
+    val optionVisible: Boolean = false,
     val sortState: SortState = SortState.Alphabet,
     val queryState: VocabularyQuery = VocabularyQuery(),
     val currentWordState: List<VocabularyImpl> = emptyList()
 )
 
 enum class SortState { Alphabet, Latest, Random }
+
+const val totalWordClassName = "전체"
