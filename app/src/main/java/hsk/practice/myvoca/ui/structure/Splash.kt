ada87fffd47de1fbb90f65415128b877639fabc5
hsk <mwy3055@gmail.com>
2021-08-18 23:00:10 +0900

    feature: Splash 개발 중
    왜 애니메이션이 안 될까?

--- app/src/main/java/hsk/practice/myvoca/ui/structure/Splash.kt
+++ app/src/main/java/hsk/practice/myvoca/ui/structure/Splash.kt
@@ -1,16 +1,31 @@
 package hsk.practice.myvoca.ui.structure
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Pages
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import hsk.practice.myvoca.ui.theme.MyVocaTheme
 import kotlinx.coroutines.delay
 
 @Composable
@@ -50,10 +65,52 @@ private fun SplashScreen(navController: NavHostController) {
         navController.navigate(MyVocaAppName)
     }
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    val t = remember { Animatable(initialValue = 1f, visibilityThreshold = 10f) }
+
+    val titleSize by animateFloatAsState(
+        targetValue = 3f,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioLowBouncy,
+            stiffness = Spring.StiffnessMedium
+        )
+    )
+
+    LaunchedEffect(true) {
+        t.animateTo(
+            targetValue = 3f,
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioLowBouncy,
+                stiffness = Spring.StiffnessMedium
+            )
+        )
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Splash?")
+        Text(
+            text = "나만의 단어장",
+            style = MaterialTheme.typography.h4
+        )
+        Text(
+            text = "MyVoca",
+            style = MaterialTheme.typography.h4
+        )
+        Icon(
+            imageVector = Icons.Outlined.Pages,
+            contentDescription = null,
+            modifier = Modifier.scale(t.value)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SplashScreenPreview() {
+    MyVocaTheme {
+        SplashScreen(navController = rememberNavController())
     }
 }
