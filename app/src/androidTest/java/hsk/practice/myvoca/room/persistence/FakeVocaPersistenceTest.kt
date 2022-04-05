e95107b62d9f282a471a7b31f4bae9adb8dd73b7
Wooyoung Myung <mwy3055@gmail.com>
2022-04-05 21:13:02 +0900

    move Persistence test code to AndroidTest, add hilt injection and clear code

--- app/src/androidTest/java/hsk/practice/myvoca/room/persistence/FakeVocaPersistenceTest.kt
+++ app/src/androidTest/java/hsk/practice/myvoca/room/persistence/FakeVocaPersistenceTest.kt
@@ -4,38 +4,69 @@ import com.hsk.data.Meaning
 import com.hsk.data.Vocabulary
 import com.hsk.data.VocabularyQuery
 import com.hsk.data.WordClass
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import hsk.practice.myvoca.room.TestAfterClear
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(JUnit4::class)
-class FakeVocaPersistenceTest {
+@HiltAndroidTest
+class FakeVocaPersistenceTest : TestAfterClear {
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var persistence: FakeVocaPersistence
+
+    @Before
+    fun initTest() {
+        hiltRule.inject()
+    }
 
     @Test
-    fun testVocabularyIsInserted() = runBlocking {
+    fun testVocabularyIsInserted() = testAfterClear {
         val expected = getExampleVoca()
-        val persistence = getPersistence()
+//        val persistence = getPersistence()
         persistence.insertVocabulary(listOf(expected))
         val actual = persistence.getVocabularyById(expected.id)
         assertEquals(expected, actual)
     }
 
     @Test
-    fun testVocabularyQuery_notExists() = runBlocking {
-        val persistence = getPersistence()
+    fun testVocabulariesIsInserted() = testAfterClear {
+        val expected = (3..10).map {
+            getExampleVoca(id = it)
+        }
+        persistence.insertVocabulary(expected)
+        val actual = getFirstAllVocabulary()
+        assertEquals(expected, actual)
+    }
+
+    private suspend fun getFirstAllVocabulary() = persistence.getAllVocabulary().first()
+
+    @Test
+    fun testVocabularyQuery_notExists() = testAfterClear {
         val expected = listOf<Vocabulary>()
         val actual = persistence.getVocabulary(VocabularyQuery(word = "empty"))
         assertEquals(expected, actual)
     }
 
     @Test
-    fun testVocabularyQuery_insertThenQuery() = runBlocking {
+    fun testVocabularyQuery_insertThenQuery() = testAfterClear {
         val voca = getExampleVoca()
         val expected = listOf(voca)
 
-        val persistence = getPersistence()
         persistence.insertVocabulary(expected)
 
         val query = VocabularyQuery(word = voca.eng)
@@ -44,10 +75,8 @@ class FakeVocaPersistenceTest {
     }
 
     @Test
-    fun testUpdateVocabulary() = runBlocking {
+    fun testUpdateVocabulary() = testAfterClear {
         val voca = getExampleVoca()
-
-        val persistence = getPersistence()
         persistence.insertVocabulary(listOf(voca))
 
         val newVoca = voca.copy(eng = "android")
@@ -60,10 +89,9 @@ class FakeVocaPersistenceTest {
     }
 
     @Test
-    fun testDeleteVocabulary() = runBlocking {
+    fun testDeleteVocabulary() = testAfterClear {
         val voca = getExampleVoca()
 
-        val persistence = getPersistence()
         persistence.insertVocabulary(listOf(voca))
         persistence.deleteVocabulary(listOf(voca))
 
@@ -74,13 +102,21 @@ class FakeVocaPersistenceTest {
         assertEquals(expected, actual)
     }
 
-    private fun getExampleVoca() = Vocabulary.create(
-        id = 3,
-        eng = "test",
-        meaning = listOf(Meaning(WordClass.NOUN, "테스트")),
-        memo = ""
+    private fun getExampleVoca(
+        id: Int = 3,
+        eng: String = "test",
+        meaning: List<Meaning> = listOf(Meaning(WordClass.NOUN, "테스트")),
+        memo: String = ""
+    ) = Vocabulary.create(
+        id = id,
+        eng = eng,
+        meaning = meaning,
+        memo = memo
     )
 
-    private fun getPersistence() = FakeVocaPersistence()
+    override fun clear() = runBlocking {
+        val allVoca = persistence.getAllVocabulary().first()
+        persistence.deleteVocabulary(allVoca)
+    }
 
 }
