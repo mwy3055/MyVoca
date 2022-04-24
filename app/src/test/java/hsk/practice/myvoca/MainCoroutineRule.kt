1ba8d142329283b431a7eb7419352ff53ddc61f5
Wooyoung Myung <mwy3055@gmail.com>
2022-04-24 23:11:25 +0900

    refactor: change dispatcher type to TestDispatcher

--- app/src/test/java/hsk/practice/myvoca/MainCoroutineRule.kt
+++ app/src/test/java/hsk/practice/myvoca/MainCoroutineRule.kt
@@ -1,16 +1,16 @@
 package hsk.practice.myvoca
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class MainCoroutineRule(private val dispatcher: CoroutineDispatcher) : TestWatcher() {
+class MainCoroutineRule(private val dispatcher: TestDispatcher) : TestWatcher() {
     override fun starting(description: Description?) {
         super.starting(description)
         Dispatchers.setMain(dispatcher)
@@ -18,7 +18,7 @@ class MainCoroutineRule(private val dispatcher: CoroutineDispatcher) : TestWatch
 
     override fun finished(description: Description?) {
         super.finished(description)
-        Dispatchers.resetMain()
         dispatcher.cancel()
+        Dispatchers.resetMain()
     }
 }
