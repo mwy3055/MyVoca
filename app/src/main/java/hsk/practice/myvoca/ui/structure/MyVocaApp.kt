7c8f64af8e1bd0ac9467ead676fc095f5286a73e
Wooyoung Myung <mwy3055@gmail.com>
2022-05-01 20:04:54 +0900

    refactor: general compose refactoring (names, functions, etc.)

--- app/src/main/java/hsk/practice/myvoca/ui/structure/MyVocaApp.kt
+++ app/src/main/java/hsk/practice/myvoca/ui/structure/MyVocaApp.kt
@@ -6,9 +6,11 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -27,15 +29,8 @@ import hsk.practice.myvoca.ui.screens.quiz.QuizViewModel
 import hsk.practice.myvoca.ui.theme.MyVocaTheme
 
 @Composable
-fun MyVocaApp(onLaunch:suspend ()->Unit = {}) {
+fun MyVocaApp(onLaunch: suspend () -> Unit = {}) {
     MyVocaTheme {
-        val allScreens = MyVocaScreen.values().toList()
-        val navController = rememberNavController()
-        val backstackEntry = navController.currentBackStackEntryAsState()
-        val currentScreen = MyVocaScreen.fromRoute(
-            backstackEntry.value?.destination?.route
-        )
-
         val systemUiController = rememberSystemUiController()
         val systemBarColor = MaterialTheme.colors.primarySurface
         LaunchedEffect(key1 = true) {
@@ -45,6 +40,10 @@ fun MyVocaApp(onLaunch:suspend ()->Unit = {}) {
             )
         }
 
+        val allScreens = MyVocaScreen.values().toList()
+        val navController = rememberNavController()
+        val backStackEntry by navController.currentBackStackEntryAsState()
+        val currentScreen = MyVocaScreen.fromRoute(backStackEntry?.destination?.route)
         Scaffold(
             topBar = {
                 MyVocaTopAppBar(currentScreen = currentScreen)
@@ -54,11 +53,7 @@ fun MyVocaApp(onLaunch:suspend ()->Unit = {}) {
                     allScreens = allScreens,
                     onTabSelected = { screen ->
                         if (screen != currentScreen) {
-                            navController.navigate(screen.name) {
-                                if (screen == MyVocaScreen.Home) {
-                                    popUpTo(MyVocaScreen.Home.name)
-                                }
-                            }
+                            navigateTo(navController, screen)
                         }
                     },
                     currentScreen = currentScreen
@@ -73,22 +68,37 @@ fun MyVocaApp(onLaunch:suspend ()->Unit = {}) {
     }
 }
 
+private fun navigateTo(
+    navController: NavHostController,
+    screen: MyVocaScreen
+) {
+    navController.navigate(screen.name) {
+        if (screen == MyVocaScreen.Home) {
+            clearPopUpStack()
+        }
+    }
+}
+
+private fun NavOptionsBuilder.clearPopUpStack() {
+    popUpTo(MyVocaScreen.Home.name)
+}
+
 @Composable
 fun MyVocaNavGraph(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
+    homeViewModel: HomeViewModel = hiltViewModel(),
+    allWordViewModel: AllWordViewModel = hiltViewModel(),
+    quizViewModel: QuizViewModel = hiltViewModel(),
+    profileViewModel: ProfileViewModel = hiltViewModel(),
 ) {
-    // 여기서 만들면 화면 전환에 상관없이 유지된다.
-    val homeViewModel: HomeViewModel = hiltViewModel()
-    val allWordViewModel: AllWordViewModel = hiltViewModel()
-    val quizViewModel: QuizViewModel = hiltViewModel()
-    val profileViewModel: ProfileViewModel = hiltViewModel()
+    // NavHost 밖에서 만들면 화면 전환에 상관없이 유지된다.
     NavHost(
         navController = navController,
         startDestination = MyVocaScreen.Home.name,
         modifier = modifier
     ) {
-        // 여기서 만들면 화면을 전환할 때마다 매번 새로운 객체가 생성된다.
+        // NavHost 안에서 만들면 매번 새로운 객체가 생성된다.
         composable(MyVocaScreen.Home.name) {
             HomeScreen(homeViewModel)
         }
