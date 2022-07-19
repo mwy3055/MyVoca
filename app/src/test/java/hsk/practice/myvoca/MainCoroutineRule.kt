f74e33304f611bb46e08575b1d7521eef97ecc90
Wooyoung Myung <mwy3055@gmail.com>
2022-07-19 18:03:44 +0900

    refactor: remove unnecessary interface

--- app/src/test/java/hsk/practice/myvoca/MainCoroutineRule.kt
+++ app/src/test/java/hsk/practice/myvoca/MainCoroutineRule.kt
@@ -4,13 +4,12 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
-import org.junit.rules.TestRule
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 import kotlin.coroutines.CoroutineContext
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class MainCoroutineRule(private val dispatcher: TestDispatcher) : TestWatcher(), TestRule {
+class MainCoroutineRule(private val dispatcher: TestDispatcher) : TestWatcher() {
     override fun starting(description: Description?) {
         super.starting(description)
         Dispatchers.setMain(dispatcher)
