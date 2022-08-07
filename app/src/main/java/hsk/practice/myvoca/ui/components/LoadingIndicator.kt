c12bf6f338cd2e8b8bbfc2394c8c713f4662cbc4
Wooyoung Myung <mwy3055@gmail.com>
2022-08-07 20:58:33 +0900

    refactor: add some parameters to circle loading indicator, such as background color, circle size, etc.

--- app/src/main/java/hsk/practice/myvoca/ui/components/LoadingIndicator.kt
+++ app/src/main/java/hsk/practice/myvoca/ui/components/LoadingIndicator.kt
@@ -1,14 +1,34 @@
 package hsk.practice.myvoca.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.zIndex
 
 @Composable
-fun LoadingIndicator(modifier: Modifier = Modifier) {
-    CircularProgressIndicator(
+fun LoadingIndicator(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = Color.Gray.copy(alpha = 0.6f),
+    circleAlign: Alignment = Alignment.Center,
+    circleFraction: Float = 0.35f
+) {
+    Box(
         modifier = modifier
+            .fillMaxSize()
+            .background(color = backgroundColor)
             .zIndex(1f)
-    )
+            .clickable(false) {} // to prevent click event from behind elements
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .align(circleAlign)
+                .fillMaxSize(fraction = circleFraction)
+        )
+    }
 }
