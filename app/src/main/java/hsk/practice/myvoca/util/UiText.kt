04b1ac99ae8e85d48f89d1c877c1f1d7f0c0130f
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-06 01:06:28 +0900

    UiText sealed class 수정
    string resource 에 변수가 조합되는 케이스 대응,
    Composable 에서의 asString() 함수와 non-Composable 에서의 asString() 함수를 분리

--- app/src/main/java/hsk/practice/myvoca/util/UiText.kt
+++ app/src/main/java/hsk/practice/myvoca/util/UiText.kt
@@ -2,13 +2,25 @@ package hsk.practice.myvoca.util
 
 import android.content.Context
 import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 sealed class UiText {
-    class StringResource(@StringRes val resId: Int) : UiText()
     data class DirectString(val value: String) : UiText()
 
-    fun asString(context: Context) = when (this) {
-        is StringResource -> context.getString(resId)
+    class StringResource(
+        @StringRes val resId: Int,
+        vararg val args: Any
+    ) : UiText()
+
+    @Composable
+    fun asString() = when (this) {
         is DirectString -> value
+        is StringResource -> stringResource(resId, *args)
+    }
+
+    fun asString(context: Context) = when (this) {
+        is DirectString -> value
+        is StringResource -> context.getString(resId, *args)
     }
 }
