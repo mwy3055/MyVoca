9f5e93c1705ca9289496519a76ae6198309b3fa1
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-05 23:44:09 +0900

    UiText sealed class 추가

---
+++ app/src/main/java/hsk/practice/myvoca/util/UiText.kt
@@ -0,0 +1,14 @@
+package hsk.practice.myvoca.util
+
+import android.content.Context
+import androidx.annotation.StringRes
+
+sealed class UiText {
+    class StringResource(@StringRes val resId: Int) : UiText()
+    data class DirectString(val value: String) : UiText()
+
+    fun asString(context: Context) = when (this) {
+        is StringResource -> context.getString(resId)
+        is DirectString -> value
+    }
+}
